c35aacc84d9f0d89df0f478e45a6b5609775d238
Anshuman Solanki <anshumanreal399@gmail.com>
2020-10-06 23:03:02 +0530

    Blog Post complete

--- components/layout.js
+++ components/layout.js
@@ -1,9 +1,10 @@
+import React from 'react'
 import Head from 'next/head'
 import styles from './layout.module.css'
 import utilStyles from '../styles/utils.module.css'
 import Link from 'next/link'
 
-const name = '[Your Name]'
+const name = 'Anshuman Solanki'
 export const siteTitle = 'Next.js Sample Website'
 
 export default function Layout({ children, home }) {
